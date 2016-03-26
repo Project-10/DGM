ef115371e140896c4c624c2ba0f1dc0d5d94724d
Sergey Kosov <sergey.kosov@project-10.de>
2016-03-26 12:30:55 +0100

    up Signed-off-by: ereator <sergey.kosov@project-10.de>

---
+++ Demo 1D/exact.cpp
@@ -0,0 +1,107 @@
+#include "exact.h"
+
+const byte   nStates = 2;						// {false, true}
+const size_t nNodes  = 4;						// four students
+
+void CExact::fillGraph(CGraph *graph)
+{
+	Mat nodePot(nStates, 1, CV_32FC1);			// node Potential (column-vector)
+	Mat edgePot(nStates, nStates, CV_32FC1);	// edge Potential (matrix)
+
+	// Setting the node potentials
+	for (size_t i = 0; i < nNodes; i++) {
+		if (i % 2) {							// for odd nodes
+			nodePot.at<float>(0, 0) = 0.10f;	// nPot = (0.10; 0.90)^T
+			nodePot.at<float>(1, 0) = 0.90f;
+		}
+		else {									// for even nodes
+			nodePot.at<float>(0, 0) = 0.75f;	// nPot = (0.75; 0.25)^T
+			nodePot.at<float>(1, 0) = 0.25f;
+		}
+		graph->setNode(i, nodePot);
+	}
+
+	// Defying the edge potential matrix
+	edgePot.at<float>(0, 0) = 2.0f;	edgePot.at<float>(0, 1) = 1.0f;
+	edgePot.at<float>(1, 0) = 1.0f;	edgePot.at<float>(1, 1) = 2.0f;
+
+	// Setting the edges potentials
+	for (size_t i = 0; i < nNodes - 1; i++)
+		graph->setArk(i, i + 1, edgePot);
+}
+
+void CExact::printMarginals(CGraph *graph, char * str)
+{
+	Mat		 nodePot;
+	printf("%s:\t", str);
+	for (int i = 0; i < nNodes; i++) {
+		graph->getNode(i, nodePot);
+		printf((i < nNodes - 1) ? " %.2f\t%.2f\t|" : " %.2f\t%.2f\n", nodePot.at<float>(0, 0), nodePot.at<float>(1, 0));
+	}
+}
+
+void CExact::Main(void)
+{
+	size_t	  i;
+	CGraph	* graph				= new CGraph(nStates);
+	CDecode * decoderExcact		= new CDecodeExact(graph);
+	CInfer  * infererExact		= new CInferExact(graph);
+	CInfer  * infererChain		= new CInferChain(graph);
+	CInfer  * infererTree		= new CInferTree(graph);
+	CInfer  * infererLBP		= new CInferLBP(graph);
+	CInfer  * infererViterbi	= new CInferViterbi(graph);
+
+	// Building the graph
+	for (i = 0; i < nNodes; i++)		graph->addNode();
+	for (i = 0; i < nNodes - 1; i++)	graph->addArk(i, i + 1);
+
+	printf("\t\t\t\tINFERENCE\n\t");
+	for (i = 0; i < nNodes - 1; i++) printf("   Node %zd\t|", i); printf("   Node %zd\n\t", i);
+	for (i = 0; i < nNodes - 1; i++) printf(" False\tTrue\t|"); printf(" False\tTrue\n");
+	printf("--------------------------------------------------------------------\n");
+
+	fillGraph(graph);											// Without training, filling the graph with pre-defined potentials
+	printMarginals(graph, "Init");								// Initial potentials
+
+	byte *decoding_decoderExcact = decoderExcact->decode();		// Exact decoding from decode
+
+	infererExact->infer();										// Exact inference
+	byte *decoding_infererExcact = infererExact->decode();		// Exact decoding from inferer
+	printMarginals(graph, "Exact");
+
+	fillGraph(graph);
+	infererChain->infer();										// Chain inference
+	byte *decoding_infererChain = infererChain->decode();		// Chain decoding
+	printMarginals(graph, "Chain");
+
+	fillGraph(graph);
+	infererTree->infer();										// Tree inference
+	byte *decoding_infererTree = infererTree->decode();			// Tree decoding
+	printMarginals(graph, "Tree");
+
+	fillGraph(graph);
+	infererLBP->infer(10);										// Loopy Belief Probagation inference
+	byte *decoding_infererLBP = infererLBP->decode();			// Loopy Belief Probagation decoding
+	printMarginals(graph, "LBP");
+
+	fillGraph(graph);
+	infererViterbi->infer(10);									// Viterbi inference
+	byte *decoding_infererViterbi = infererViterbi->decode();	// Viterbi decoding
+	printMarginals(graph, "Vtrbi");
+
+	printf("\n\t\t\t\tDECODING\n\t\t");
+	for (i = 0; i < nNodes - 1; i++) printf("Node%zd\t", i); printf("Node%zd\n", i);
+	printf("---------------------------------------------\n");
+	printf("Exact decoder:\t");	for (i = 0; i < nNodes; i++) printf(decoding_decoderExcact[i]  ? "true\t" : "false\t");	printf("\n");
+	printf("Exact inferer:\t");	for (i = 0; i < nNodes; i++) printf(decoding_infererExcact[i]  ? "true\t" : "false\t");	printf("\n");
+	printf("LBP inferer:\t");	for (i = 0; i < nNodes; i++) printf(decoding_infererLBP[i]     ? "true\t" : "false\t");	printf("\n");
+	printf("Vtrbi inferer:\t");	for (i = 0; i < nNodes; i++) printf(decoding_infererViterbi[i] ? "true\t" : "false\t"); printf("\n");
+
+	delete graph;
+	delete decoderExcact;
+	delete infererExact;
+	delete infererChain;
+	delete infererTree;
+	delete infererLBP;
+	delete infererViterbi;
+}
---
+++ Demo 1D/main.cpp
@@ -0,0 +1,42 @@
+#include "Demo1D.h"
+#include "exact.h"
+#include "chain.h"
+#include "tree.h"
+
+void print_help(void)
+{
+	printf("Usage: \"Demo 1D.exe\" <app>\n");
+	printf("where <app> is on of the following:\n");
+	printf("  exact - for \n");
+	printf("  chain - for \n");
+	printf("  tree  - for \n");
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		print_help();
+		return 0;
+	}
+	std::string arg = argv[1];
+	
+	CDemo1D * demo = NULL;
+
+	// Chosing demo
+	if (arg == "exact") demo = new CExact();
+	if (arg == "chain") demo = new CChain();
+	if (arg == "tree")  demo = new CTree();
+	if (!demo) {
+		print_help();
+		return 0;
+	}
+
+	demo->Main();
+
+	// Exiting
+	printf("\nPress <Enter> key to exit...");
+	getchar();
+
+	delete demo;
+	return 0;
+}
