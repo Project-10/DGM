a6569307dfe737c590d22a44b3eb488d029ecbad
Sergey Kosov <sergey.kosov@project-10.de>
2016-04-20 03:06:26 +0200

    + HSV feature

---
+++ FEX/HSV.cpp
@@ -0,0 +1,13 @@
+#include "HSV.h"
+#include "macroses.h"
+
+namespace DirectGraphicalModels { namespace fex
+{
+Mat CHSV::get(const Mat &img)
+{
+	DGM_ASSERT_MSG(img.channels() == 3, "Input image has %d channel(s), but must have 3.", img.channels());
+	Mat res;
+	cvtColor(img, res, CV_BGR2HSV);
+	return res;
+}
+} }
---
+++ FEX/HSV.h
@@ -0,0 +1,34 @@
+// Hue, Saturation and Value feature extraction class interface
+// Written by Sergey G. Kosov in 2016 for Project X
+#pragma once
+
+#include "BaseFeatureExtractor.h"
+
+namespace DirectGraphicalModels { namespace fex
+{
+	// ================================ Saturation Class ==============================
+	/**
+	* @brief Hue, Saturation and Value feature extraction class
+	* @author Sergey G. Kosov, sergey.kosov@project-10.de
+	*/
+	class CHSV : public CBaseFeatureExtractor
+	{
+	public:
+		/**
+		* @brief Constructor.
+		* @param img Input image of type \b CV_8UC3.
+		*/
+		DllExport CHSV(Mat &img) : CBaseFeatureExtractor(img) {}
+		DllExport virtual ~CHSV(void) {}
+
+		DllExport virtual Mat get(void) const { return get(m_img); }
+
+		/**
+		* @brief Extracts the HSV feature.
+		* @details This function transforms the input image into HSV (hue-saturation-value) color space.
+		* @param img Input image of type \b CV_8UC3.
+		* @return The (hue-saturation-value) feature image of type \b CV_8UC3.
+		*/
+		DllExport static Mat get(const Mat &img);
+	};
+} }
