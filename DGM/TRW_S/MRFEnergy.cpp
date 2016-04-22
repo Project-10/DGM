ff551ddb0b9227668fdffbc52c7bd8f07ce77be4
Sergey Kosov <ereator@mail.ru>
2016-04-23 01:07:16 +0200

    Update MRFEnergy.cpp
    code standardization

--- DGM/TRW_S/MRFEnergy.cpp
+++ DGM/TRW_S/MRFEnergy.cpp
@@ -12,6 +12,7 @@ void DefaultErrorFn(char* msg)
 	exit(1);
 }
 
+// Constructor
 template <class T> MRFEnergy<T>::MRFEnergy(GlobalSize Kglobal, ErrorFunction errorFn)
 	: m_errorFn(errorFn ? errorFn : DefaultErrorFn),
 	  m_mallocBlockFirst(NULL),
@@ -23,14 +24,12 @@ template <class T> MRFEnergy<T>::MRFEnergy(GlobalSize Kglobal, ErrorFunction err
 	  m_vectorMaxSizeInBytes(0),
 	  m_isEnergyConstructionCompleted(false),
 	  m_buf(NULL)
-{
-}
+{ }
 
-//template <class T> MRFEnergy<T>::~MRFEnergy<T>()
+// Destructor
 template <class T> MRFEnergy<T>::~MRFEnergy()
 {
-	while (m_mallocBlockFirst)
-	{
+	while (m_mallocBlockFirst) {
 		MallocBlock* next = m_mallocBlockFirst->m_next;
 		delete m_mallocBlockFirst;
 		m_mallocBlockFirst = next;
@@ -70,20 +69,12 @@ template <class T> void MRFEnergy<T>::AddNodeData(NodeId i, NodeData data)
 
 template <class T> void MRFEnergy<T>::AddEdge(NodeId i, NodeId j, EdgeData data)
 {
-	if (m_isEnergyConstructionCompleted)
-	{
-		m_errorFn("Error in AddNode(): graph construction completed - nodes cannot be added");
-	}
-
-	MRFEdge* e;
-
+	if (m_isEnergyConstructionCompleted) m_errorFn("Error in AddNode(): graph construction completed - nodes cannot be added");
 	int actualEdgeSize = Edge::GetSizeInBytes(m_Kglobal, i->m_K, j->m_K, data);
-	if (actualEdgeSize < 0)
-	{
-		m_errorFn("Error in AddEdge() (invalid parameter?)");
-	}
+	if (actualEdgeSize < 0) m_errorFn("Error in AddEdge() (invalid parameter?)");
+	
 	int MRFedgeSize = sizeof(MRFEdge) - sizeof(Edge) + actualEdgeSize;
-	e = (MRFEdge*) Malloc(MRFedgeSize);
+	MRFEdge *e = (MRFEdge*) Malloc(MRFedgeSize);
 
 	e->m_message.Initialize(m_Kglobal, i->m_K, j->m_K, data, &i->m_D, &j->m_D);
 
@@ -102,71 +93,50 @@ template <class T> void MRFEnergy<T>::AddEdge(NodeId i, NodeId j, EdgeData data)
 
 template <class T> void MRFEnergy<T>::ZeroMessages()
 {
-	Node* i;
-	MRFEdge* e;
+	Node	* i;
+	MRFEdge	* e;
 
-	if (!m_isEnergyConstructionCompleted)
-	{
-		CompleteGraphConstruction();
-	}
-
-	for (i=m_nodeFirst; i; i=i->m_next)
-	{
-		for (e=i->m_firstForward; e; e=e->m_nextForward)
-		{
+	if (!m_isEnergyConstructionCompleted) CompleteGraphConstruction();
+	for (i = m_nodeFirst; i; i = i->m_next)
+		for (e = i->m_firstForward; e; e = e->m_nextForward)
 			e->m_message.GetMessagePtr()->SetZero(m_Kglobal, i->m_K);
-		}
-	}
 }
 
 template <class T> void MRFEnergy<T>::AddRandomMessages(unsigned int random_seed, REAL min_value, REAL max_value)
 {
-	Node* i;
-	MRFEdge* e;
-	int k;
+	Node	* i;
+	MRFEdge	* e;
+	int 	  k;
 
-	if (!m_isEnergyConstructionCompleted)
-	{
-		CompleteGraphConstruction();
-	}
+	if (!m_isEnergyConstructionCompleted) CompleteGraphConstruction();
 
 	srand(random_seed);
 
-	for (i=m_nodeFirst; i; i=i->m_next)
-	{
-		for (e=i->m_firstForward; e; e=e->m_nextForward)
-		{
+	for (i = m_nodeFirst; i; i = i->m_next) 
+		for (e = i->m_firstForward; e; e = e->m_nextForward) {
 			Vector* M = e->m_message.GetMessagePtr();
-			for (k=0; k<M->GetArraySize(m_Kglobal, i->m_K); k++)
-			{
+			for (k = 0; k<M->GetArraySize(m_Kglobal, i->m_K); k++) {
 				REAL x = (REAL)( min_value + rand()/((double)RAND_MAX) * (max_value - min_value) );
 				x += M->GetArrayValue(m_Kglobal, i->m_K, k);
 				M->SetArrayValue(m_Kglobal, i->m_K, k, x);
 			}
 		}
-	}
 }
 
 /////////////////////////////////////////////////////////////////////////////////
 
 template <class T> void MRFEnergy<T>::CompleteGraphConstruction()
 {
-	Node* i;
-	Node* j;
-	MRFEdge* e;
-	MRFEdge* ePrev;
+	Node	* i;
+	Node	* j;
+	MRFEdge	* e;
+	MRFEdge	* ePrev;
 
-	if (m_isEnergyConstructionCompleted)
-	{
-		m_errorFn("Fatal error in CompleteGraphConstruction");
-	}
+	if (m_isEnergyConstructionCompleted) m_errorFn("Fatal error in CompleteGraphConstruction");
 
 	printf("Completing graph construction... ");
 
-	if (m_buf)
-	{
-		m_errorFn("CompleteGraphConstruction(): fatal error");
-	}
+	if (m_buf) m_errorFn("CompleteGraphConstruction(): fatal error");
 
 	m_buf = (char *) Malloc(m_vectorMaxSizeInBytes + 
 		( m_vectorMaxSizeInBytes > Edge::GetBufSizeInBytes(m_vectorMaxSizeInBytes) ?
@@ -175,8 +145,7 @@ template <class T> void MRFEnergy<T>::CompleteGraphConstruction()
 	// set forward and backward edges properly
 #ifdef _DEBUG
 	int ordering;
-	for (i=m_nodeFirst, ordering=0; i; i=i->m_next, ordering++)
-	{
+	for (i = m_nodeFirst, ordering = 0; i; i = i->m_next, ordering++) {
 		if ( (i->m_ordering != ordering)
 		  || (i->m_ordering == 0 && i->m_prev)
 		  || (i->m_ordering != 0 && i->m_prev->m_ordering != ordering-1) )
@@ -184,47 +153,31 @@ template <class T> void MRFEnergy<T>::CompleteGraphConstruction()
 			m_errorFn("CompleteGraphConstruction(): fatal error (wrong ordering)");
 		}
 	}
-	if (ordering != m_nodeNum)
-	{
-		m_errorFn("CompleteGraphConstruction(): fatal error");
-	}
+	if (ordering != m_nodeNum) m_errorFn("CompleteGraphConstruction(): fatal error");
+
 #endif
-	for (i=m_nodeFirst; i; i=i->m_next)
-	{
-		i->m_firstBackward = NULL;
-	}
-	for (i=m_nodeFirst; i; i=i->m_next)
-	{
+	for (i = m_nodeFirst; i; i = i->m_next) i->m_firstBackward = NULL;
+	for (i = m_nodeFirst; i; i = i->m_next) {
 		ePrev = NULL;
-		for (e=i->m_firstForward; e; )
-		{
+		for (e = i->m_firstForward; e; ) {
 			assert(i == e->m_tail);
 			j = e->m_head;
 
-			if (i->m_ordering < j->m_ordering)
-			{
+			if (i->m_ordering < j->m_ordering) {
 				e->m_nextBackward = j->m_firstBackward;
 				j->m_firstBackward = e;
 
 				ePrev = e;
 				e = e->m_nextForward;
-			}
-			else
-			{
+			} else {
 				e->m_message.Swap(m_Kglobal, i->m_K, j->m_K);
 				e->m_tail = j;
 				e->m_head = i;
 
 				MRFEdge* eNext = e->m_nextForward;
 
-				if (ePrev)
-				{
-					ePrev->m_nextForward = e->m_nextForward;
-				}
-				else
-				{
-					i->m_firstForward = e->m_nextForward;
-				}
+				if (ePrev) ePrev->m_nextForward = e->m_nextForward;
+				else i->m_firstForward = e->m_nextForward;
 
 				e->m_nextForward = j->m_firstForward;
 				j->m_firstForward = e;
