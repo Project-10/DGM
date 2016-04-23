c67aa1f730aed5b3e0e2dc9ef8ceea894573c1a2
Sergey Kosov <ereator@mail.ru>
2016-04-23 21:46:02 +0200

    Update minimize.cpp

--- DGM/TRW_S/minimize.cpp
+++ DGM/TRW_S/minimize.cpp
@@ -12,11 +12,9 @@ template <class T> int MRFEnergy<T>::Minimize_TRW_S(Options &options, REAL &lowe
 	Node	* j;
 	MRFEdge	* e;
 	REAL	  vMin;
-	int		  iter;
 	REAL	  lowerBoundPrev;
 
-	if (!m_isEnergyConstructionCompleted)
-		CompleteGraphConstruction();
+	if (!m_isEnergyConstructionCompleted) CompleteGraphConstruction();
 
 	printf("TRW_S algorithm\n");
 
@@ -25,8 +23,8 @@ template <class T> int MRFEnergy<T>::Minimize_TRW_S(Options &options, REAL &lowe
 	Vector	* Di	= (Vector *) m_buf;
 	void	* buf	= (void *) (m_buf + m_vectorMaxSizeInBytes);
 
-	iter = 0;
-	bool lastIter = false;
+	int		iter		= 0;
+	bool	lastIter	= false;
 
 	// main loop
 	for (iter = 1; ; iter++) {
@@ -238,37 +236,32 @@ template <class T> int MRFEnergy<T>::Minimize_BP(Options& options, REAL& energy,
 
 template <class T> typename T::REAL MRFEnergy<T>::ComputeSolutionAndEnergy()
 {
-	Node* i;
-	Node* j;
-	MRFEdge* e;
-	REAL E = 0;
+	Node	* i;
+	Node	* j;
+	MRFEdge	* e;
+	REAL 	  E = 0;
 
-	Vector* DiBackward = (Vector*) m_buf; // cost of backward edges plus Di at the node
-	Vector* Di = (Vector*) (m_buf + m_vectorMaxSizeInBytes); // all edges plus Di at the node
+	Vector	* DiBackward = (Vector*) m_buf; 					// cost of backward edges plus Di at the node
+	Vector	* Di = (Vector*) (m_buf + m_vectorMaxSizeInBytes); 	// all edges plus Di at the node
 
-	for (i=m_nodeFirst; i; i=i->m_next)
-	{
+	for (i = m_nodeFirst; i; i = i->m_next) {
 		// Set Ebackward[ki] to be the sum of V(ki,j->m_solution) for backward edges (i,j).
 		// Set Di[ki] to be the value of the energy corresponding to
 		// part of the graph considered so far, assuming that nodes u
 		// in this subgraph are fixed to u->m_solution
 
 		DiBackward->Copy(m_Kglobal, i->m_K, &i->m_D);
-		for (e=i->m_firstBackward; e; e=e->m_nextBackward)
-		{
+		for (e = i->m_firstBackward; e; e = e->m_nextBackward) {
 			assert(i == e->m_head);
 			j = e->m_tail;
-
 			e->m_message.AddColumn(m_Kglobal, j->m_K, i->m_K, j->m_solution, DiBackward, 0);
 		}
 
 		// add forward edges
 		Di->Copy(m_Kglobal, i->m_K, DiBackward);
 
-		for (e=i->m_firstForward; e; e=e->m_nextForward)
-		{
+		for (e = i->m_firstForward; e; e = e->m_nextForward)
 			Di->Add(m_Kglobal, i->m_K, e->m_message.GetMessagePtr());
-		}
 
 		Di->ComputeMin(m_Kglobal, i->m_K, i->m_solution);
 
