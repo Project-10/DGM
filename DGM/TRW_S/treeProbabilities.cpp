7929190080a1a2f9bb56d9203bc39d0caf8db94e
Sergey Kosov <ereator@mail.ru>
2016-04-23 21:32:58 +0200

    Update treeProbabilities.cpp

--- DGM/TRW_S/treeProbabilities.cpp
+++ DGM/TRW_S/treeProbabilities.cpp
@@ -7,44 +7,23 @@
 
 #include "instances.inc"
 
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-
 template <class T> void MRFEnergy<T>::SetMonotonicTrees()
 {
-	Node* i;
-	MRFEdge* e;
+	Node	* i;
+	MRFEdge	* e;
 
-	if (!m_isEnergyConstructionCompleted)
-	{
-		CompleteGraphConstruction();
-	}
+	if (!m_isEnergyConstructionCompleted) CompleteGraphConstruction();
 
-	for (i=m_nodeFirst; i; i=i->m_next)
-	{
-		REAL mu;
-
+	for (i = m_nodeFirst; i; i = i->m_next) {
 		int nForward = 0, nBackward = 0;
-		for (e=i->m_firstForward; e; e=e->m_nextForward)
-		{
-			nForward ++;
-		}
-		for (e=i->m_firstBackward; e; e=e->m_nextBackward)
-		{
-			nBackward ++;
-		}
+		for (e = i->m_firstForward;  e; e = e->m_nextForward) 	nForward++;
+		for (e = i->m_firstBackward; e; e = e->m_nextBackward)	nBackward++;
+
 		int ni = (nForward > nBackward) ? nForward : nBackward;
 
-		mu = (REAL)1 / ni;
-		for (e=i->m_firstBackward; e; e=e->m_nextBackward)
-		{
-			e->m_gammaBackward = mu;
-		}
-		for (e=i->m_firstForward; e; e=e->m_nextForward)
-		{
-			e->m_gammaForward = mu;
-		}
+		REAL mu = (REAL) 1 / ni;
+		for (e = i->m_firstBackward; e; e = e->m_nextBackward)	e->m_gammaBackward = mu;
+		for (e = i->m_firstForward;  e; e = e->m_nextForward)	e->m_gammaForward  = mu;
 	}
 }
 
