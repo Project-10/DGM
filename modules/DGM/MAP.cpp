cdeec3869e5451629512711fd127a03c00e5440f
Sergey Kosov <sergey.kosov@project-10.de>
2016-11-29 14:33:22 +0100

    Bug in Average Precision estimation fixed

--- modules/DGM/MAP.cpp
+++ modules/DGM/MAP.cpp
@@ -3,25 +3,35 @@
 
 namespace DirectGraphicalModels 
 {
-	float getMAP(const vec_byte_t &predictions, const vec_float_t &potentials, byte gt)
+	float getMAP(const vec_byte_t &predictions, const vec_float_t &potentials, const vec_byte_t &gt, byte state)
 	{
 		// Assertions
 		DGM_ASSERT(predictions.size() == potentials.size());
+		DGM_ASSERT(predictions.size() == gt.size());
 		
-		std::vector<std::pair<byte, float>> container;
+		std::vector<std::tuple<byte, float, byte>> container;			// prediction | pot | gt
 		for (size_t i = 0; i < predictions.size(); i++)
-			container.push_back(std::make_pair(predictions[i], potentials[i]));
-		std::sort(container.begin(), container.end(), [](auto &left, auto &right) { return left.second > right.second; });
-
-		float	res = 0.0f;
-		int		sum	= 0;								// number of correct predictions
-		int		i = 0;
+			container.push_back(std::make_tuple(predictions[i], potentials[i], gt[i]));
+		std::sort(container.begin(), container.end(), [](auto const &left, auto const &right) { return std::get<1>(left) > std::get<1>(right); });
+	
+		float	res				= 0.0f;
+		int		nRelevants		= 0;					// number of elevant elements
+		int		nCoincidences	= 0;					// number of correct predictions
+		int		i				= 0;
 		for (auto &c : container) {
 			i++;
-			if (c.first == gt) res += static_cast<float>(++sum) / i;
+			if (std::get<2>(c) == state) {				// if (gt == state)
+				nRelevants++;
+				if (std::get<0>(c) == state) {			// if (prediction = state)
+					nCoincidences++;
+					res += static_cast<float>(nCoincidences) / i;
+				}
+			}
 		}
 		
-		if (sum > 0) res /= sum;
+		DGM_IF_WARNING(nRelevants == 0, "There are no states <state> in <gt>");
+
+		if (nRelevants > 0) res /= nRelevants;
 
 		return res;
 	}
--- modules/DGM/MAP.h
+++ modules/DGM/MAP.h
@@ -1,4 +1,4 @@
-// Mean Average Precision class
+// Average Precision class
 // Written by Sergey Kosov for Project X in 2016
 #pragma once
 
@@ -8,12 +8,13 @@ namespace DirectGraphicalModels
 {
 	/**
 	* @ingroup moduleEva
-	* @brief Returns <a href="https://www.youtube.com/watch?v=pM6DJ0ZZee0" target="_blank">Mean Average Precision (MAP)</a> for the selected state (class) \b gt
+	* @brief Returns <a href="https://www.youtube.com/watch?v=pM6DJ0ZZee0" target="_blank">Average Precision</a> for the selected state (class) \b state
 	* @param predictions The most probable configuration, returned by the CDecode::decode() function
-	* @param potentials potential values for each node of the graph, returned by the CInfer::getPotentials() function
-	* @param gt The groundtruth state (class)
-	* @returns The MAP value
+	* @param potentials The potential values for each node of the graph, returned by the CInfer::getPotentials() function
+	* @param gt The groundtruth values for each node of the graph 
+	* @param state The state (class) for which the Average Precision is calculated
+	* @returns The Average Precision value
 	*/	
-	DllExport float getMAP(const vec_byte_t &predictions, const vec_float_t &potentials, byte gt);
+	DllExport float getMAP(const vec_byte_t &predictions, const vec_float_t &potentials, const vec_byte_t &gt, byte state);
 
 }
