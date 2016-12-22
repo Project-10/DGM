377e3cad590f6737d534d81a0f0141fcd4a3bef7
ereator <sergey.kosov@project-10.de>
2016-12-22 01:15:48 +0100

    copyOpenCVDLL.bat +flann

--- batch/copyOpenCVDLL.bat
+++ batch/copyOpenCVDLL.bat
@@ -5,6 +5,7 @@ SET OpenCV_src=%OPENCVDIR%\build\x64\vc14\bin
 @echo on
 copy %OpenCV_src%\opencv_core%version%.dll .\
 copy %OpenCV_src%\opencv_features2d%version%.dll .\
+copy %OpenCV_src%\opencv_flann%version%.dll .\
 copy %OpenCV_src%\opencv_highgui%version%.dll .\
 copy %OpenCV_src%\opencv_imgproc%version%.dll .\
 copy %OpenCV_src%\opencv_imgcodecs%version%.dll .\
@@ -13,6 +14,7 @@ copy %OpenCV_src%\opencv_ml%version%.dll .\
                 
 copy %OpenCV_src%\opencv_core%version%d.dll .\
 copy %OpenCV_src%\opencv_features2d%version%d.dll .\
+copy %OpenCV_src%\opencv_flann%version%d.dll .\
 copy %OpenCV_src%\opencv_highgui%version%d.dll .\
 copy %OpenCV_src%\opencv_imgproc%version%d.dll .\
 copy %OpenCV_src%\opencv_imgcodecs%version%d.dll .\
