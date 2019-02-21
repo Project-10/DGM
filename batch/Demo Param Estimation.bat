ff7d97d7dd599929b5707f8fd9d9d7f64db3e4af
Sergey Kosov <sergey.kosov@project-10.de>
2019-02-21 02:28:26 +0100

    Added batch files for Demo Param Estimation

--- batch/Demo Param Estimation.bat
+++ batch/Demo Param Estimation.bat
@@ -5,5 +5,5 @@ if exist ..\Data\001_img.jpg if exist ..\Data\001_gt.bmp if exist ..\Data\002_im
 	if not exist ..\Data\002_fv.jpg (
 		"Demo Feature Extraction.exe" ..\Data\002_img.jpg ..\Data\002_fv.jpg
 	)
-	"Demo Dense.exe" ..\Data\001_fv.jpg ..\Data\001_gt.bmp ..\Data\002_fv.jpg ..\Data\002_gt.bmp ..\Data\002_img.jpg .\output_denseCRF.jpg
+	"Demo Param Estimation.exe" ..\Data\001_fv.jpg ..\Data\001_gt.bmp ..\Data\002_fv.jpg ..\Data\002_gt.bmp ..\Data\002_img.jpg
 )
