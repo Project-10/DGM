7bce55b70e084d0cfbfa00f50920ec06910f137e
Sergey Kosov <sergey.kosov@project-10.de>
2019-08-11 01:46:39 +0200

    switch to smart pointer

--- modules/DGM/InferChain.cpp
+++ modules/DGM/InferChain.cpp
@@ -5,13 +5,12 @@ namespace DirectGraphicalModels
 {
 	void CInferChain::calculateMessages(unsigned int)
 	{
-		const byte nStates = getGraph().getNumStates();
 		float *temp = new float[getGraph().getNumNodes()];
 
 		// Forward pass
 		std::for_each(getGraphPairwise().m_vNodes.begin(), getGraphPairwise().m_vNodes.end() - 1, [&](ptr_node_t &node) {
 			for (size_t e_t : node->to) {									// outgoing edges
-				Edge *edge_to = getGraphPairwise().m_vEdges[e_t].get();		// current outgoing edge
+				ptr_edge_t &edge_to = getGraphPairwise().m_vEdges[e_t];			// current outgoing edge
 				if (edge_to->node2 == node->id + 1)
 					calculateMessage(*edge_to, temp, getMessage(e_t));
 			} // e_t;
