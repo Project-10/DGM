95ce48530b20d3c90b1f7e9117f8f3a00c8c319e
Sergey Kosov <ereator@mail.ru>
2021-04-08 15:55:56 +0200

    Delete DNN.h

--- include/DNN.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "DNN/Neuron.h"
-// #include "DNN/Functions.hpp"
-
-/**
-@defgroup moduleDNN DNN Module
-@section sec_dnn_title Deep Neural Network Module
-allows for building basic deep learning applications.
-*/
