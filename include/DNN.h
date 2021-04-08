00cb75570851099e4242cfa48091afd9bcb7e3f4
Sergey Kosov <ereator@mail.ru>
2021-04-08 15:58:55 +0200

    Revert "Delete DNN.h"
    This reverts commit 95ce48530b20d3c90b1f7e9117f8f3a00c8c319e.

---
+++ include/DNN.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "DNN/Neuron.h"
+// #include "DNN/Functions.hpp"
+
+/**
+@defgroup moduleDNN DNN Module
+@section sec_dnn_title Deep Neural Network Module
+allows for building basic deep learning applications.
+*/
