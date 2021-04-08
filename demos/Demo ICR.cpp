f8958a09b4dad4a5059962a5081f16fafef8f27f
diondermaku <55454268+diondermaku@users.noreply.github.com>
2021-04-08 16:00:24 +0200

    Initial update CNeuron class (#36)
    * Test 1
    
    * Neural net + Backpropagation
    
    this is a draft for the NeuralNet with the backpropagation technique
    
    * neuraNet-V.1.0
    
    * v1.0
    
    remove whitespace
    
    * v1.1
    
    retrigger build
    
    * v1.2
    
    try solving issues
    
    * Update Demo ICR.cpp
    
    v1.3
    
    * Final code revision
    
    - reduce Demo_ICR file size
    - create a file with functions
    - more comments for functions
    - update code
    
    * Remove whitespace
    
    remove whitespace
    
    * Update Neuron.h
    
    try to resolve issues
    
    * resolve issue
    
    try to resolve issue 2
    
    * remove files
    
    remove files
    
    * all issues resolved
    
    * New Changes
    
    -remove default constructor
    -add constructor with param
    
    * Delete train_digit.txt
    
    * Delete train_data.txt
    
    * Delete test_data.txt
    
    * Delete test_digits.txt
    
    * Delete DNN.h
    
    * Revert "Delete DNN.h"
    
    This reverts commit 95ce48530b20d3c90b1f7e9117f8f3a00c8c319e.
    
    Co-authored-by: Sergey Kosov <ereator@mail.ru>

--- demos/Demo ICR.cpp
+++ demos/Demo ICR.cpp
@@ -1,9 +1,166 @@
-#include "DGM.h"
 #include "DNN.h"
-
+#include <fstream>
 namespace dgm = DirectGraphicalModels;
+using namespace std::chrono;
+
+//dgm::dnn::CNeuron* dotProd(int hiddenLayer, int inputLayer, dgm::dnn::CNeuron A[], dgm::dnn::CNeuron B[]);
 
 int main() {
-	dgm::dnn::CNeuron neuron;
+    const int inputLayer   = 784;
+    const int hiddenLayer  = 60;
+    const int outputLayer  = 10;
+    float     rangeMin     = -0.5;
+    float     rangeMax     = 0.5;
+
+    std::vector<std::shared_ptr<dgm::dnn::CNeuron>> vpMyNeuron;
+    std::vector<std::shared_ptr<dgm::dnn::CNeuron>> vpMyHiddenNeuron;
+    std::vector<std::shared_ptr<dgm::dnn::CNeuron>> vpMyOutputNeuron;
+    dgm::dnn::CNeuron *myNeuron = new dgm::dnn::CNeuron(inputLayer);
+    dgm::dnn::CNeuron *myHiddenNeuron = new dgm::dnn::CNeuron(hiddenLayer);
+    dgm::dnn::CNeuron *myOutputNeuron = new dgm::dnn::CNeuron(outputLayer);
+
+
+//    dgm::dnn::CNeuron *myNeuron        = new dgm::dnn::CNeuron[inputLayer];
+//    dgm::dnn::CNeuron *myHiddenNeuron  = new dgm::dnn::CNeuron[hiddenLayer];
+//    dgm::dnn::CNeuron *myOutputNeuron  = new dgm::dnn::CNeuron[outputLayer];
+
+//    int *trainDataDigit  = readDigitData("../../../test_digits.txt", 2000);
+//    int **trainDataBin   = readBinData("../../../test_data.txt", 2000);
+//    int **resultsArray   = resultPredictions(outputLayer);
+//
+//// ==================== Training Data ====================
+//    auto startTraining = high_resolution_clock::now();
+//
+//    for(int i = 0; i < inputLayer; i++) {
+//        myNeuron[i].generateWeights(hiddenLayer);
+//    }
+//    for(int i = 0; i < hiddenLayer; i++) {
+//        myHiddenNeuron[i].generateWeights(outputLayer);
+//    }
+//
+//        for(int k = 0; k < 2000; k++) {
+//            //std::cout<<"Training for digit: "<< trainDataDigit[k]<<std::endl;
+//            for(int i = 0; i < inputLayer; i++) {
+//                float val = (float)trainDataBin[k][i]/255;
+//                float value = (int)(val * 1000 + .5);
+//                myNeuron[i].setNodeValue( (float)value / 1000 );
+//            }
+//
+//            myHiddenNeuron = dotProd(hiddenLayer, inputLayer, myNeuron, myHiddenNeuron);
+//            myOutputNeuron = dotProd(outputLayer, hiddenLayer, myHiddenNeuron, myOutputNeuron);
+//
+//            double *resultErrorRate = new double[outputLayer];
+//            for(int i=0 ; i < outputLayer; i++) {
+//                resultErrorRate[i] = resultsArray[trainDataDigit[k]][i] - myOutputNeuron[i].getNodeValue();
+//            }
+//
+//        // ==================== BACKPROPAGATION ====================
+//            float (*DeltaWjk)[outputLayer]  = new float[hiddenLayer][outputLayer];
+//            float (*DeltaVjk)[hiddenLayer]  = new float[inputLayer][hiddenLayer];
+//            float *DeltaIn_j                = new float[hiddenLayer];
+//            float *DeltaJ                   = new float[hiddenLayer];
+//            float learningRate              = 0.1;
+//
+//            //updates weights between [hiddenLayer][outputLayer]
+//            for(int i = 0; i < hiddenLayer; i++) {
+//                double val = 0;
+//                for(int j = 0; j < outputLayer; j++) {
+//                    val += myHiddenNeuron[i].getWeight(j) * resultErrorRate[j];
+//                    DeltaWjk[i][j] = learningRate * resultErrorRate[j] * myHiddenNeuron[i].getNodeValue();
+//                }
+//                DeltaIn_j[i] = val;
+//            }
+//
+//            //still hiddenlayer nodes
+//            for(int i = 0; i < hiddenLayer; i++) {
+//                float sigmoid = 1 / (1 + exp(myHiddenNeuron[i].getNodeValue()));
+//                float inverse = 1 - sigmoid;
+//                DeltaJ[i] = DeltaIn_j[i] * sigmoid * inverse;
+//            }
+//
+//            //updates weights between [inputLayer][hiddenLayer]
+//            for(int i = 0; i < inputLayer; i++) {
+//                for(int j = 0; j < hiddenLayer; j++) {
+//                    DeltaVjk[i][j] = learningRate * DeltaJ[j] * myNeuron[i].getNodeValue();
+//                }
+//            }
+//
+//        // ==================== UPDATE WEIGHTS ====================
+//            for(int i = 0; i < inputLayer; i++) {
+//                for(int j = 0; j < hiddenLayer; j++) {
+//                    float oldWeight = myNeuron[i].getWeight(j);
+//                    myNeuron[i].setWeight(j, oldWeight + DeltaVjk[i][j]);
+//                }
+//            }
+////
+//            for(int i = 0; i < hiddenLayer; i++) {
+//                for(int j = 0; j < outputLayer; j++) {
+//                    float oldWeight = myHiddenNeuron[i].getWeight(j);
+//                    myHiddenNeuron[i].setWeight(j, oldWeight + DeltaWjk[i][j]);
+//                }
+//            }
+//        }
+//        auto stopTraining = high_resolution_clock::now();
+////
+//
+//
+//
+//    // ==================== TEST DIGITS ====================
+//        int testDataSize    = 2000;
+//        int *testDataDigit  = readDigitData("../../../train_digit.txt", testDataSize);
+//        int **testDataBin   = readBinData("../../../train_data.txt", testDataSize);
+//        int correct         = 0;
+//        int uncorrect       = 0;
+//
+//        auto startTesting = high_resolution_clock::now();
+//        for(int z = 0; z < testDataSize; z++) {
+//            for(int i = 0; i < inputLayer; i++) {
+//                float val = (float)testDataBin[z][i]/255;
+//                float value = (int)(val * 1000 + .5);
+//                myNeuron[i].setNodeValue( (float)value / 1000 );
+//            }
+//
+//            myHiddenNeuron = dotProd(hiddenLayer, inputLayer, myNeuron, myHiddenNeuron);
+//            myOutputNeuron = dotProd(outputLayer, hiddenLayer, myHiddenNeuron, myOutputNeuron);
+//
+//            double *allPredictionsforDigits = new double[outputLayer];
+//            for(int i=0 ; i < outputLayer; i++) {
+//                allPredictionsforDigits[i] = myOutputNeuron[i].getNodeValue();
+//            }
+//
+//            float maxAccuracy = 0;
+//            int number;
+//            for(int i=0 ; i < outputLayer; i++){
+//                if(allPredictionsforDigits[i] >= maxAccuracy){
+//                    maxAccuracy = allPredictionsforDigits[i];
+//                    number = i;
+//                }
+//            }
+//    //        std::cout<<"prediction "<<"["<<number<<"] for digit " <<testDataDigit[z] <<" with "<<maxAccuracy<<"% at position: "<<z<<std::setw(5);
+//            (number == testDataDigit[z]) ? correct++ : uncorrect++;
+//        }
+//
+//        auto stopTesting = high_resolution_clock::now();
+//        auto durationTest = duration_cast<milliseconds>(stopTesting - startTesting);
+//        auto durationTrain = duration_cast<milliseconds>(stopTraining - startTraining);
+//
+//        std::cout << "Time taken to train data: "<< durationTrain.count() << " miliseconds" << std::endl;
+//        std::cout << "Time taken to test data: "<< durationTest.count()<< " miliseconds" << std::endl;
+//
+//        std::cout << "poz: " << correct << std::endl << "neg: " << uncorrect << std::endl;
+//        std::cout << "average: " << (float)correct/(correct+uncorrect)*100 << "%" << std::endl;
 	return 0;
 }
+
+
+//dgm::dnn::CNeuron* dotProd(int hiddenLayer, int inputLayer, dgm::dnn::CNeuron A[], dgm::dnn::CNeuron B[]) {
+//    for(int i=0 ; i < hiddenLayer; i++) {
+//        double val = 0;
+//        for(int j=0; j < inputLayer ; j++) {
+//           val += A[j].getWeight(i) * A[j].getNodeValue();
+//        }
+//        float value = applySigmoidFunction(val);
+//        B[i].setNodeValue(value);
+//    }
+//    return B;
+//}
