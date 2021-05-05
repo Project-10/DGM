91969026618b5c52b42a5243bf08c78a6ee5440e
diondermaku <55454268+diondermaku@users.noreply.github.com>
2021-05-05 03:09:46 +0200

    Perceptron 89% accuracy (#41)
    * Test 1
    
    * Neural net + Backpropagation
    
    this is a draft for the NeuralNet with the backpropagation technique
    
    * neuraNet-V.1.0
    
    * v1.0
    
    remove whitespace
    
    * v1.1
    
    retrigger build
    
    * v1.2
    
    try solving issues
    
    * Update Demo ICR.cpp
    
    v1.3
    
    * Final code revision
    
    - reduce Demo_ICR file size
    - create a file with functions
    - more comments for functions
    - update code
    
    * Remove whitespace
    
    remove whitespace
    
    * Update Neuron.h
    
    try to resolve issues
    
    * resolve issue
    
    try to resolve issue 2
    
    * remove files
    
    remove files
    
    * all issues resolved
    
    * New Changes
    
    -remove default constructor
    -add constructor with param
    
    * Delete train_digit.txt
    
    * Delete train_data.txt
    
    * Delete test_data.txt
    
    * Delete test_digits.txt
    
    * Delete DNN.h
    
    * Revert "Delete DNN.h"
    
    This reverts commit 95ce48530b20d3c90b1f7e9117f8f3a00c8c319e.
    
    * New version
    
    -use smart pointers
    -push neurons in vectors
    -convert data to images
    -train & test data from images 28x28
    
    * resolve 2 CodeFactor issues
    
    * remove redundant code
    
    - clean code a bit
    - add the labels for the digits
    
    * Refactor code
    
    - remove redundant code
    - use a dotProduct function
    - reduce the length of backpropagation algorithm
    - remove functions to neuron.cpp
    - leave function declarations in .h file
    - add comments for the fucntions
    
    * try to pass github check
    
    * Remove DemoICR, Neuron.cpp, and Neuron.h
    
    -remove files so we can merge the test/train images
    
    * Add DemoICR, Neuron.h, Neuron.cpp
    
    - Demo ICR is mostly commented so the build passes
    
    * Remove train/test data
    
    * review changes
    
    - changed the way how to read images (0001)
    
    * Move methods from Neuron.h to Demo ICR (temorary)
    
    * Add dgm::Timer
    
    * create function for backpropagation
    
    * trigger build
    
    * Refactor readDigitData to readGroundTruth
    
    * try to resolve CodeFactor issue
    
    * use Printf instead of cout
    
    * Remove int **resultPredictions(int outputLayer)
    
    * Remove resultsArray variable and clean up the code
    
    * Refactoring readImgData() method
    
    * Refactoring the back-propagation function
    
    * Add Confusion Matrix to DemoICR
    
    * Remove readImgData() method
    
    * Add CNeuronLayer class
    
    * Update NeuronLayer.cpp
    
    * use vector to find accuracy
    
    - use max_element to find accuracy
    - use std::distance to find index
    
    * Add CNeuronLayerMat and CPerceptron classes
    
    Incorporate the num of features and num of states into the project
    
    * Switching to matrices in NeuronLayerMat
    
    * change dotProd to Matrix multiplication
    
    - this reduced the training & testing time by half
    
    * remove whitespace
    
    * uncomment getWeights
    
    * Change bacprop to Matrix multiplication
    
    - if you see a way to refactor backpropagation, please do so
    
    * add gemm()
    
    * Removed a modified file from pull request
    
    * Simplify dotProd() method
    
    * Refactoring together the backPropagate() method
    
    * Perceptron with 89% accuracy
    
    - add 3 epochs
    - use sigmoid derivative
    - use 100 hidden nodes
    
    * remove whitespace
    
    Co-authored-by: Sergey Kosov <ereator@mail.ru>
    Co-authored-by: Sergey Kosov <sergey.kosov@project-10.de>

--- demos/Demo ICR.cpp
+++ demos/Demo ICR.cpp
@@ -6,7 +6,11 @@
 
 namespace dgm = DirectGraphicalModels;
 
-
+float sigmoidFunction_derivative(float x)
+{
+    float s = 1.0f / (1.0f + expf(-x));
+    return s * (1 - s);
+}
 /**
  * Reads the digits numerical value in a decimal notation
  *
@@ -15,109 +19,119 @@ namespace dgm = DirectGraphicalModels;
  */
 std::vector<byte> readGroundTruth(const std::string& fileName)
 {
-	std::vector<byte> res;
-	std::ifstream inFile;
-	inFile.open(fileName.c_str());
-
-	if (inFile.is_open()) {
-		int val;
-		while (!inFile.eof()) {
-			inFile >> val;
-			res.push_back(static_cast<byte>(val));
-		}
-		inFile.close();
-	}
-	return res;
+    std::vector<byte> res;
+    std::ifstream inFile;
+    inFile.open(fileName.c_str());
+    
+    if (inFile.is_open()) {
+        int val;
+        while (!inFile.eof()) {
+            inFile >> val;
+            res.push_back(static_cast<byte>(val));
+        }
+        inFile.close();
+    }
+    return res;
 }
 
 int main()
 {
-	const byte		nStates					= 10;				// 10 digits (number of output nodes)
-	const word		nFeatures				= 28 * 28;			// every pixel of 28x28 digit image is a distinct feature (number of input nodes)
-    const size_t    numNeuronsHiddenLayer	= 60;
-    const size_t	numTrainSamples  		= 4000;
-	const size_t 	numTestSamples    		= 2000;
+    const byte      nStates                  = 10; // 10 digits (number of output nodes)
+    const word      nFeatures                = 28 * 28; // every pixel of 28x28 digit image
+    const size_t    numNeuronsHiddenLayer    = 100;
+    const size_t    numTrainSamples          = 3000;
+    const size_t    numTestSamples           = 2000;
+    const size_t    epochs                   = 3;
 
 #ifdef WIN32
-	const std::string dataPath = "../../data/digits/";
+    const std::string dataPath = "../../data/digits/";
 #else
-	const std::string dataPath = "../../../data/digits/";
+    const std::string dataPath = "../../../data/digits/";
 #endif
 
-    dgm::dnn::CNeuronLayerBias layerInput(nFeatures, 0);
-    dgm::dnn::CNeuronLayerBias layerHidden(numNeuronsHiddenLayer, nFeatures);
-    dgm::dnn::CNeuronLayerBias layerOutput(nStates, numNeuronsHiddenLayer);
- 
-	layerHidden.generateRandomWeights();
-	layerOutput.generateRandomWeights();
+    dgm::dnn::CNeuronLayerMat layerInput(nFeatures, numNeuronsHiddenLayer);
+    dgm::dnn::CNeuronLayerMat layerHidden(numNeuronsHiddenLayer, nStates);
+    dgm::dnn::CNeuronLayerMat layerOutput(nStates, 0);
 
-	Mat fv;
+    layerInput.generateRandomWeights();
+    layerHidden.generateRandomWeights();
 
-	// ==================== TRAINING DIGITS ====================
-	dgm::Timer::start("Training...");
-	auto	trainGT = readGroundTruth(dataPath + "train_gt.txt");
-	for(int s = 0; s < numTrainSamples; s++) {
+    Mat fv;
 
-		std::stringstream ss;
-		ss << dataPath << "train/digit_" << std::setfill('0') << std::setw(4) << s << ".png";
-		std::string fileName = samples::findFile(ss.str());
-		Mat img = imread(fileName, 0);
-		img = img.reshape(1, img.cols * img.rows);
-		img.convertTo(fv, CV_32FC1, 1.0 / 255);
-		fv = Scalar(1.0f) - fv;
+    // ==================== TRAINING DIGITS ====================
+    dgm::Timer::start("Training...");
+    auto    trainGT = readGroundTruth(dataPath + "train_gt.txt");
 
-		layerInput.setValues(fv);
+    for(size_t x = 0; x < epochs; x++){
+        for(size_t s = 0; s < numTrainSamples; s++) {
+            std::stringstream ss;
+            ss << dataPath << "train/digit_" << std::setfill('0') << std::setw(4) << s << ".png";
+            std::string fileName = samples::findFile(ss.str());
+            Mat img = imread(fileName, 0);
+            img = img.reshape(1, img.cols * img.rows);
+            img.convertTo(fv, CV_32FC1, 1.0 / 255);
+            fv = Scalar(1.0f) - fv;
 
-		layerHidden.dotProd(layerInput);
-		layerOutput.dotProd(layerHidden);
+            layerInput.setValues(fv);
 
-		Mat outputValues = layerOutput.getValues();
+            layerHidden.dotProd(layerInput);
+            layerHidden.applySigmoid();
 
-        Mat resultErrorRate(nStates, 1, CV_32FC1);
-		for(int i = 0; i < resultErrorRate.rows; i++) 
-			resultErrorRate.at<float>(i, 0) = (trainGT[s] == i) ? 1 : 0;
-		resultErrorRate -= outputValues;
+            layerOutput.dotProd(layerHidden);
+            Mat outputValues = layerOutput.getValues();
+            layerOutput.applySigmoid();
 
-        dgm::dnn::CNeuronLayerBias::backPropagate(layerInput, layerHidden, layerOutput, resultErrorRate, 0.1f);
-    } // samples
-	dgm::Timer::stop();
+            Mat resultErrorRate(nStates, 1, CV_32FC1);
 
-	// ==================== TESTING DIGITS ====================
-	dgm::CCMat confMat(nStates);
-	dgm::Timer::start("Testing...");
-	auto 	testGT = readGroundTruth(dataPath + "test_gt.txt");
-	for(size_t s = 0; s < numTestSamples; s++) {
-		std::stringstream ss;
-		ss << dataPath << "test/digit_" << std::setfill('0') << std::setw(4) << s << ".png";
-		std::string fileName = samples::findFile(ss.str());
-		Mat img = imread(fileName, 0);
-		img = img.reshape(1, img.cols * img.rows);
-		img.convertTo(fv, CV_32FC1, 1.0 / 255);
-		fv = Scalar(1.0f) - fv;
+            for(int i = 0; i < resultErrorRate.rows; i++) {
+                resultErrorRate.at<float>(i, 0) = (trainGT[s] == i) ? 1 : 0;
+                resultErrorRate.at<float>(i, 0) = (resultErrorRate.at<float>(i, 0) - outputValues.at<float>(i,0)) * sigmoidFunction_derivative(outputValues.at<float>(i,0));
+            }
+            dgm::dnn::CNeuronLayerMat::backPropagate(layerInput, layerHidden, layerOutput, resultErrorRate, 0.1f);
+        }
+    }
+    dgm::Timer::stop();
 
-		layerInput.setValues(fv);
-		layerHidden.dotProd(layerInput);
-		layerOutput.dotProd(layerHidden);
-        
+    // ==================== TESTING DIGITS ====================
+    dgm::CCMat confMat(nStates);
+    dgm::Timer::start("Testing...");
+    auto     testGT = readGroundTruth(dataPath + "test_gt.txt");
+    for(size_t s = 0; s < numTestSamples; s++) {
+        std::stringstream ss;
+        ss << dataPath << "test/digit_" << std::setfill('0') << std::setw(4) << s << ".png";
+        std::string fileName = samples::findFile(ss.str());
+        Mat img = imread(fileName, 0);
+        img = img.reshape(1, img.cols * img.rows);
+        img.convertTo(fv, CV_32FC1, 1.0 / 255);
+        fv = Scalar(1.0f) - fv;
+
+        layerInput.setValues(fv);
+
+        layerHidden.dotProd(layerInput);
+        layerHidden.applySigmoid();
+
+        layerOutput.dotProd(layerHidden);
+        layerOutput.applySigmoid();
+
         std::vector<double>pot = layerOutput.getValues();
 
         auto maxAccuracy = max_element(std::begin(pot), std::end(pot));
         int number = std::distance(pot.begin(), maxAccuracy);
-        
-		confMat.estimate(number, testGT[s]);
+
+        confMat.estimate(number, testGT[s]);
+
         //printf("prediction [%d] for digit %d with %.3f%s at position %zu \n", number, testDataDigit[z], maxAccuracy, "%", z);
-	} // samples
-	dgm::Timer::stop();
-	printf("Accuracy = %.2f%%\n", confMat.getAccuracy());
-	
-	// Confusion matrix
-	dgm::vis::CMarker marker;
-	Mat cMat    = confMat.getConfusionMatrix();
-	Mat cMatImg = marker.drawConfusionMatrix(cMat, dgm::vis::MARK_BW);
-	imshow("Confusion Matrix", cMatImg);
-	
-	waitKey();
-	
-	
-	return 0;
+    }
+    dgm::Timer::stop();
+    printf("Accuracy = %.2f%%\n", confMat.getAccuracy());
+
+    // Confusion matrix
+    dgm::vis::CMarker marker;
+    Mat cMat    = confMat.getConfusionMatrix();
+    Mat cMatImg = marker.drawConfusionMatrix(cMat, dgm::vis::MARK_BW);
+    imshow("Confusion Matrix", cMatImg);
+
+    waitKey();
+
+    return 0;
 }
