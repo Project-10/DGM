bac597059202b5ec1df8ba5f015a16bd14b6ec07
Shahin Mammadov <71777445+shahinmv@users.noreply.github.com>
2021-06-29 21:19:07 +0200

    Added support for multiple hidden layers
    Modified CPerceptron constructor and backpropagate function to support arbitrary number of hidden layers.

--- modules/DNN/Perceptron.cpp
+++ modules/DNN/Perceptron.cpp
@@ -6,52 +6,68 @@ namespace DirectGraphicalModels {
 		// Constructor
 		CPerceptron::CPerceptron(const std::vector<int>& vNumNeurons) {
 			// TODO: imp,lement this constructor in the future
-			for (size_t i = 0; i < vNumNeurons.size(); i++) {
+			for (size_t i = 0; i < vNumNeurons.size(); i++){
 				int numNeurons = vNumNeurons[i];
 				int numConnections = (i == 0) ? 0 : vNumNeurons[i - 1];
 				ptr_nl_t pNeuronLayer = std::make_shared<CNeuronLayer>(numNeurons, numConnections, [](float x) {return x; }, [](float x) { return 1; });
 				m_vpNeuronLayers.push_back(pNeuronLayer);
 			}
 		}
-		
+
 		// Constructor
-		CPerceptron::CPerceptron(const std::vector<ptr_nl_t>& vpLayers) 
+		CPerceptron::CPerceptron(const std::vector<ptr_nl_t>& vpLayers, const std::vector<ptr_nl_t>& numHiddenLayer)
 		{
-			for (auto& nl : vpLayers)
-				m_vpNeuronLayers.push_back(nl);
+			//for (auto& nl : vpLayers)
+			//	m_vpNeuronLayers.push_back(nl);
+
+			m_vpNeuronLayers.push_back(vpLayers[0]);
+			
+			for (int i = 0; i < numHiddenLayer.size(); i++) 
+				m_vpNeuronLayers.push_back(numHiddenLayer[i]);
+
+			m_vpNeuronLayers.push_back(vpLayers[1]);
 		}
 
-		Mat	CPerceptron::getPrediction(const Mat& inputValues) 
+		Mat	CPerceptron::getPrediction(const Mat& inputValues)
 		{
 			// Asserions
 			DGM_ASSERT(m_vpNeuronLayers.size() > 1);
-			
+
 			// Initialize the values of the input layer
-			m_vpNeuronLayers[0]->setNetValues(inputValues);
+			m_vpNeuronLayers[0]->setNetValues(inputValues); // input layer
 
 			// Calculate values of the nodes on all other layers
-			for (size_t i = 1; i < m_vpNeuronLayers.size(); i++)
-				m_vpNeuronLayers[i]->dotProd(m_vpNeuronLayers[i - 1]->getValues());
+			for (size_t i = 1; i < m_vpNeuronLayers.size(); i++) {
+				m_vpNeuronLayers[i]->dotProd(m_vpNeuronLayers[i - 1]->getValues()); // determining values of hidden layers neurons
+			}
 
 			// Return the node values from the output layer
-			return m_vpNeuronLayers.back()->getValues();
+			return m_vpNeuronLayers.back()->getValues(); 
 		}
 
-		// TODO: this method works only for 3 layers
-		void CPerceptron::backPropagate(const Mat& solution, const Mat& gt, float learningRate)
+		// TODO: this method works for multiple layers, solution = guesses, gt = answers
+		void CPerceptron::backPropagate(const Mat& solution, const Mat& gt, float learningRate, int numHiddenLayer)
 		{
-			Mat error = gt - solution;
-			for (int i = 0; i < error.rows; i++)
-				error.at<float>(i, 0) *= m_vpNeuronLayers[2]->getActivationFunctionDeriateve()(solution.at<float>(i, 0));
+			int numLayers = m_vpNeuronLayers.size() - 1; // number of layers [0, 1, 2 ... n]
 			
-			Mat DeltaIn_j; // = layerC.getWeights() x error;
-			gemm(m_vpNeuronLayers[2]->getWeights(), error, 1, Mat(), 0, DeltaIn_j);
+			Mat error = gt - solution;
+			for (int i = 0; i < error.rows; i++) {
+				error.at<float>(i, 0) *= m_vpNeuronLayers[numLayers]->getActivationFunctionDeriateve()(solution.at<float>(i, 0));
+			}
+			
+			std::vector<Mat> Error; //Vector containing errors for each of the layers neurons
+			Error.push_back(error);
+			
+			for (int i = 0; i < numHiddenLayer; i++)
+			{
+				Mat temp = m_vpNeuronLayers[numLayers-i]->getWeights() * Error[i]; // weights * error
+				Error.push_back(temp);
+			}
 
-			// layerC.m_weights += learningRate * layerB.m_values x error.t()
-			gemm(m_vpNeuronLayers[1]->getValues(), error.t(), learningRate, m_vpNeuronLayers[2]->getWeights(), 1, m_vpNeuronLayers[2]->getWeights());
-
-			//layerB.m_weights += learningRate * layerA.m_values x DeltaJ.t();
-			gemm(m_vpNeuronLayers[0]->getValues(), DeltaIn_j.t(), learningRate, m_vpNeuronLayers[1]->getWeights(), 1, m_vpNeuronLayers[1]->getWeights());
+			for(int i = 1; i < m_vpNeuronLayers.size(); i++)
+			{
+				gemm(m_vpNeuronLayers[numLayers - i]->getValues(), Error[i-1].t(), learningRate, m_vpNeuronLayers[m_vpNeuronLayers.size() - i]->getWeights(), 1, m_vpNeuronLayers[m_vpNeuronLayers.size() - i]->getWeights());
+			}
 		}
 	}
 }
