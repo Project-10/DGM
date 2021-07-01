bb24493a0cf5cb9798efa021c1af995a6dbf0b32
Shahin Mammadov <71777445+shahinmv@users.noreply.github.com>
2021-07-01 23:24:13 +0200

    Added function to store all layers in pLayers

--- demos/Demo ICR.cpp
+++ demos/Demo ICR.cpp
@@ -55,7 +55,7 @@ int main()
     const size_t	numTrainSamples  		= 4000;
 	const size_t 	numTestSamples    		= 2000;
 	const size_t	numEpochs				= 3;
-	const int       numHiddenLayer = 2;         //number of hidden layers -- DEFAULT = 1
+	const int       numHiddenLayer          = 1;         //number of hidden layers -- DEFAULT = 1
 #ifdef WIN32
 	const std::string dataPath = "../../data/digits/";
 #else
@@ -65,23 +65,22 @@ int main()
 	auto pLayerInput = std::make_shared<dgm::dnn::CNeuronLayer>(nFeatures, 0, [](float x) { return x; }, [](float x) { return 1.0f; });
 	auto pLayerOutput = std::make_shared<dgm::dnn::CNeuronLayer>(nStates, numNeuronsHiddenLayer, &sigmoidFunction, &sigmoidFunction_derivative);
 
-	std::vector<decltype(pLayerInput)> pLayerHidden;
+	std::vector<decltype(pLayerInput)> pLayers;
 
+	pLayers.push_back(pLayerInput);
 
 	for (int i = 0; i < numHiddenLayer; i++) //Hidden layers stored in vector
-		if (pLayerHidden.size() == 0)
-			pLayerHidden.emplace_back(std::make_shared<dgm::dnn::CNeuronLayer>(numNeuronsHiddenLayer, nFeatures, &sigmoidFunction, &sigmoidFunction_derivative)); // 1st Hidden Layer
+		if (pLayers.size() == 1)
+			pLayers.push_back(std::make_shared<dgm::dnn::CNeuronLayer>(numNeuronsHiddenLayer, nFeatures, &sigmoidFunction, &sigmoidFunction_derivative)); // 1st Hidden Layer
 		else
-			pLayerHidden.emplace_back(std::make_shared<dgm::dnn::CNeuronLayer>(numNeuronsHiddenLayer, numNeuronsHiddenLayer, &sigmoidFunction, &sigmoidFunction_derivative)); // 2-n hidden layers
+			pLayers.push_back(std::make_shared<dgm::dnn::CNeuronLayer>(numNeuronsHiddenLayer, numNeuronsHiddenLayer, &sigmoidFunction, &sigmoidFunction_derivative)); // 2-n hidden layers
 
+	pLayers.push_back(pLayerOutput);
 
-	if (numHiddenLayer != 0)
-		for (int i = 0; i < numHiddenLayer; i++)
-			pLayerHidden[i]->generateRandomWeights();
+	for (int i = 1; i < pLayers.size(); i++)
+		pLayers[i]->generateRandomWeights();
 
-	pLayerOutput->generateRandomWeights();
-
-	dgm::dnn::CPerceptron perceptron({ pLayerInput, pLayerOutput }, pLayerHidden);
+	dgm::dnn::CPerceptron perceptron(pLayers);
 
 	Mat fv;
 
