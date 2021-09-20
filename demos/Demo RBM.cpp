9c7f688806cbe97b868d2949f4f2fc9659961b78
Shahin Mammadov <71777445+shahinmv@users.noreply.github.com>
2021-09-20 13:34:19 +0400

    Added RBM demo

--- demos/Demo RBM.cpp
+++ demos/Demo RBM.cpp
@@ -19,12 +19,24 @@ float sigmoidFunction_derivative(float x)
 
 int main()
 {
+	const float learningRate             = 0.1;
+	const int stepsK                     = 1;
+	
 	const word nFeatures                 = 6;
 	const word numNeuronsHiddenLayer     = 3;
+
 
 	auto pLayerVisible = std::make_shared<dgm::dnn::CNeuronLayer>(nFeatures, 0, [](float x) { return x; }, [](float x) { return 1.0f; });
 	auto pLayerHidden  = std::make_shared<dgm::dnn::CNeuronLayer>(numNeuronsHiddenLayer, nFeatures, &sigmoidFunction, &sigmoidFunction_derivative);
 
 	pLayerHidden->generateRandomWeights();
+	pLayerVisible->copyWeights(pLayerHidden->getWeights());
+
+	dgm::dnn::CRBM rbm({ pLayerVisible, pLayerHidden });
+
+	//rbm.setVisibleBias(0.0);
+	//rbm.setHiddenBias(0.0);
+	
+
 
 }
