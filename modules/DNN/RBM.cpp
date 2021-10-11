1ee0802acdca730c81c9b2a12c80adc7e332ca63
Shahin Mammadov <71777445+shahinmv@users.noreply.github.com>
2021-10-11 17:54:41 +0400

    Added comments to RBM.cpp

--- modules/DNN/RBM.cpp
+++ modules/DNN/RBM.cpp
@@ -5,18 +5,23 @@ namespace DirectGraphicalModels {
 	namespace dnn {
 		CRBM::CRBM(const std::vector<ptr_nl_t>& vpLayers){
 			for (auto& nl : vpLayers)
-				m_vpNeuronLayers.push_back(nl);
+				m_vpNeuronLayers.push_back(nl); //adding layers to the vector
 		}
 
 		void CRBM::setVisibleBias(float value) {
-			visibleBias = value;
+			visibleBias = value;                    //setting the bias for visible layer
 		}
 
 		void CRBM::setHiddenBias(float value) {
-			hiddenBias = value;
+			hiddenBias = value;                     //setting the bias for hidden layer
 		}
 
-		Mat CRBM::feedForward(const Mat& values) {
+		Mat CRBM::feedForward(const Mat& values) { 
+			/*
+			In this function, we sample the hidden layer given the visible layer. 
+			We apply the sigmoid function to sum of visible layer multiplied by weights.
+			For now adding bias is not yet implemented.
+			*/
 			m_vpNeuronLayers[0]->setNetValues(values); //set the visible layer values
 
 			m_vpNeuronLayers[1]->dotProd(m_vpNeuronLayers[0]->getValues()); //sigmoid(sum(visible * weights))  bias not implemented yet
@@ -25,6 +30,12 @@ namespace DirectGraphicalModels {
 		}
 
 		Mat CRBM::feedBackward(void) const {
+			/*
+			Here we sample the visible layer given the hidden layer.
+			We apply the sigmoid function to sum of hidden layer multiplied by weights.
+			Adding bias is not yet implemented.
+			*/
+			
 			m_vpNeuronLayers[0]->dotProd(m_vpNeuronLayers[1]->getValues()); //sigmoid(sum(hidden * weights))  bias not implemented yet
 			//as we only have weights assigned to hidden layer, when we perform function above, it will result in a message saying
 			//no weights initialized for visible layer, as a temporary solution I will add a function in Neuron layer class so it copies
@@ -34,7 +45,11 @@ namespace DirectGraphicalModels {
 		}
 
 		void CRBM::contrastiveDivergence(const Mat& values, float learningRate, int stepsK) {
-
+			/*
+			We will apply the CD-K algorithm to change the weights and bias.
+		
+			       
+			*/
 		}
 	}
 }
